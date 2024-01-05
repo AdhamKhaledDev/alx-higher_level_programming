0f6afa3b816a4f8bb53b888365cf3ee281b093ba
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-04 16:51:05 -0800

    calculate

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import calculator_1
-
-def main():
-    a = 10
-    b = 5
-    print("Add: ", calculator_1.add(a, b))
-    print("Subtract: ", calculator_1.sub(a, b))
-    print("Multiply: ", calculator_1.mul(a, b))
-    print("Divide: ", calculator_1.div(a, b))
 
 if __name__ == "__main__":
-    main()
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
