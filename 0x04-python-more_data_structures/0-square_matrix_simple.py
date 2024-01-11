94e214963babfca558b5bfc157e933761c893489
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-10 16:15:36 -0800

    chmod

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@ A function that calculates the squared value of every integer in a matrix.
 """
 def square_matrix_simple(matrix=[]):
 	new_matrix = []
-	for submat in matrix:
-		result = list(map(lambda e: e**2, submat))
+	for neo in matrix:
+		result = list(map(lambda e: e**2, neo))
 		new_matrix.append(result)
 	return new_matrix
