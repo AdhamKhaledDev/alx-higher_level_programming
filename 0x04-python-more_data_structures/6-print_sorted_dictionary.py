6246a55b8748f36c73ba4b9edda40006e4c3ef38
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-10 16:41:43 -0800

    Tabs

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for neo in sorted (a_dictionary.keys()):
-        print("{}: {}".format (neo, a_dictionary[neo]))
+	for neo in sorted (a_dictionary.keys()):
+		print("{}: {}".format (neo, a_dictionary[neo]))
