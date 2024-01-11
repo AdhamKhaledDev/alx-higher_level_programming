cb221766ce78bddb0d1322fa2a3ad7ac8a3ad33d
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-10 16:44:54 -0800

    Modify_Error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 def print_sorted_dictionary(a_dictionary):
-	for neo in sorted (a_dictionary.keys()):
-		print("{}: {}".format (neo, a_dictionary[neo]))
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    for key in keys:
+        print("{}: {}".format(key, a_dictionary[key]))
