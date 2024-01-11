5cfe232226b31b23cfc430aefef0f2d40b88cfa2
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-10 16:54:57 -0800

    Miltiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
-    
-    new_dict = {}
-    for key, value in a_dictionary.items():
-        new_dict.update({key: (value * 2)})
-    return new_dict
+    my_dict = dict(a_dictionary)
+    for k, v in my_dict.items():
+        my_dict[k] = v * 2
+    return my_dict
