2b48481f0f30451f79c760103e9078c9e0da4d4a
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-10 17:06:17 -0800

    Advanced

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-def weight_average(input_list=[]):
-    if not input_list:
-        return 0
-
-    numerator = 0
-    denominator = 0
-
-    for pair in input_list:
-        numerator += pair[0] * pair[1]
-        denominator += pair[1]
-
-    return (numerator / denominator)
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
