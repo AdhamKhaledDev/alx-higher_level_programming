ae6aea12b83ed101f0a037ecec3ccc4deff9c058
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-22 11:41:34 -0800

    Implemented safe integer printing function

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,12 +2,12 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     
-    thunder = 0
+    x = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            thunder += 1
+            x += 1
         except (ValueError, TypeError):
             continue
     print("")
-    return (thunder)
+    return (x)
