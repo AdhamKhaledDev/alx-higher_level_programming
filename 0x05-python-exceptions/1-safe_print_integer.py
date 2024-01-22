5a00c55de25da785e8a680e8ee4efa38135afef1
AdhamKhaledDev <adhamk109@outlook.com>
2024-01-22 11:38:44 -0800

    Implemented safe integer printing function

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except ValueError:
-        return False
+def safe_print_list_integers(input_list=[], num_elements=0):
+    count = 0
+    for index in range(num_elements):
+        try:
+            print("{:d}".format(input_list[index]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count
