fd61331ad9d661a1e50b141c4536e6fb95d4379f
AdhamKhaledDev <adhamk109@outlook.com>
2024-04-11 15:48:01 -0700

    Args_Check

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,11 +2,11 @@
 function printArgumentMessage() {
     const argumentCount = arguments.length;
   
-    if (argumentCount === 0) {
-        console.log("No argument");
-      } else if (argumentCount === 1) {
-        console.log("Argument found");
-      } else {
-        console.log("Arguments found");
-      }
-    }
+    if (argumentCount === 0)
+      console.log("No argument");
+    else if (argumentCount === 1)
+      console.log("Argument found");
+    else
+      console.log("Arguments found");
+  }
+  
