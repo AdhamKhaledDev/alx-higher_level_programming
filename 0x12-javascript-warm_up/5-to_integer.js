af144201907cb95f0de74602510274acdcec3dac
AdhamKhaledDev <adhamk109@outlook.com>
2024-04-11 16:40:53 -0700

    printNumber

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-function printNumber (argument) {
-  if (isNaN(argument) || argument === undefined) {
-    console.log('Not a number');
-  } else {
-    console.log('My number:', parseInt(argument));
-  }
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv[2]));
 }
