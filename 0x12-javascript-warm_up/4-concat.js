5bb61f7179192a315ada149f6e21f31cfa9f0397
AdhamKhaledDev <adhamk109@outlook.com>
2024-04-11 16:28:26 -0700

    function printTwoArguments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,2 @@
 #!/usr/bin/node
-function printTwoArguments (arg1, arg2) {
-  if (arguments.length < 2) {
-    console.log('Error: Missing arguments. Please provide two arguments.');
-    return;
-  }
-
-  console.log(`${arg1} is ${arg2}`);
-}
-
-printTwoArguments('This', 'that');
+console.log(process.argv[2] + ' is ' + process.argv[3]);
