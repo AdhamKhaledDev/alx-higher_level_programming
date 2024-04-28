f470be595e2704c20b00220d2a7e6a7d676764b5
AdhamKhaledDev <adhamk109@outlook.com>
2024-04-28 14:51:35 -0700

    Fetch_And_Print_Status_Info

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,8 @@
 import requests
 
 
+import requests
+
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    response = requests.get("https://intranet.hbtn.io/status")
+    print(response.text)
