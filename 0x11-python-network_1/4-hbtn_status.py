731ea488e0000c388767eb0ecc1be63651d46fcf
AdhamKhaledDev <adhamk109@outlook.com>
2024-04-28 14:56:30 -0700

    Fetch_And_Print_Status_Info

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+
+
 import requests
 
 
+def main():
+    """Fetches the status of https://alx-intranet.hbtn.io/status and displays it."""
+
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Body response:")
+        for key, value in response.json().items():
+            print(f"\t- type: <class '{type(value)}'>")
+            print(f"\t- content: {value}")
+    else:
+        print(f"Error: {response.status_code}")
+
+
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    main()
