860d2261518d20e76c7a450888226a4af2e5f24c
AdhamKhaledDev <adhamk109@outlook.com>
2024-05-01 10:40:45 -0700

    network_Python_Part_One

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL passed as an argument
-# and displays the body of the response. A header variable is also sent with the request.
-
-URL=$1
-HEADER="X-School-User-Id: 98"
-
-curl -sH "$HEADER" "$URL"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
